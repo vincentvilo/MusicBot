adbbd4e8fb2678ed9a0236498d6dd85c00242290
MrWail <bryantran78@gmail.com>
2017-03-04 23:46:14 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+use strict'; // catch errors easier
+
+// Requirements
+const cfg = require('./config.js');
+const path = require('path');
+const Discord = require('discord.js');
+const http = require('http');
+const express = require('express');
+const bot = new Discord.Client();
+const app = express();
+const port = process.env.PORT || 8080; // sets server port
+
+// Server
+app.set('view engine', 'ejs'); // sets view engine to ejs
+
+app.use(express.static(path.join(__dirname, '/static'))); // serves static items in lib/static folder
+
+app.get('/', function (req, res) {
+  res.render('index'); // renders views/index.ejs
+});
+
+app.listen(port, function () {
+  console.log('Website is running on http://localhost:' + port); // prints message in console log when website is running
+});
+
+// Discord.js bot client
+bot.on('ready', () => {
+  console.log('I\'m ready to run! :)'); // prints message in console log when bot is ready
+  setInterval(() => {
+    http.get('http://sir-narwhal.herokuapp.com');
+  }, 1800000); // pings server every 30 minutes to prevent dyno from sleeping
+});
+
+bot.on('error', e => {
+  console.error(e); // prints message in console log when bot errors
+});
+
+bot.on('disconnect', () => {
+  console.log('I\'ve disconnected! Oh no! D:'); // prints message in console log when bot is disconnected
+});
+
+bot.login(cfg.token).then(() => {
+  console.log('I\'ve logged in! :D'); // prints message in console log when bot is logged in
+  bot.user.setGame('sir-narwhal.herokuapp.com'); // sets "Playing [game]" status to "Playing sir-narwhal.herokuapp.com"
+});
